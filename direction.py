d15bdc591a459f486899a26d58c6aac290e4a7d6
ShadowDragon8d6 <duhduck673e@gmail.com>
2024-03-31 19:32:49 -0400

    adding files

---
+++ direction.py
@@ -0,0 +1,7 @@
+import requests
+
+
+
+#Function to send commands to the robot
+def send_command(direction):
+    print(f"Command '{direction}' sent successfully.")
--- main.py
+++ main.py
@@ -1,47 +1,9 @@
-import cv2
-import numpy as np
-video = cv2.VideoCapture("Lane2.mp4")
-leftl = []
-rightl = []
-count = 0
-left_cascade = cv2.CascadeClassifier('left.xml')
+from GUI import window
+import os
 
-while True:
-   ret, frame = video.read()
-   if not ret:
-       break
-   frame = cv2.imread("left.png")
-   gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-   height, width = gray.shape
+file_path = "USER.db"
 
+if not os.path.exists(file_path):
+    open(file_path, 'w').close()
 
-
-   canny = cv2.Canny(gray, 100, 200)
-   faces = left_cascade.detectMultiScale(gray, 1.3, 3)
-   for (x, y, w, h) in faces:
-       cv2.rectangle(frame, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=3)
-       cv2.rectangle(canny, pt1=(x, y), pt2=(x + w, y + h), color=(0, 0, 0), thickness=-1)
-   lines = cv2.HoughLinesP(canny, 1, np.pi / 180, 50, maxLineGap=20, minLineLength=400)
-
-   intercepts = {}
-   if lines is not None:
-       for line in lines:
-           x1, y1, x2, y2 = line[0]
-
-           cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 255), 10)
-
-
-
-   cv2.imshow("Output", frame)
-   cv2.imshow("ee", canny)
-
-
-
-
-
-
-   if cv2.waitKey(0) & 0xFF == ord('q'):
-       break
-
-video.release()
-cv2.destroyAllWindows()
+window() #call window function
